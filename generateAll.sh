ec2900f00f0f0a72c39948f4b713100d273bb2cb
kisenshi <kisenshi@users.noreply.github.com>
2021-07-31 12:25:43 +0100

    Add sheriff to generateAll script

--- generateAll.sh
+++ generateAll.sh
@@ -24,6 +24,9 @@ python mapElitesData.py B2
 echo "python mapElitesData.py B3"
 python mapElitesData.py B3
 
+echo "python mapElitesData.py S4"
+python mapElitesData.py S4
+
 for experiment in a b c d f g h
     do
         echo "python mapElitesData.py Z5/config30Z1$experiment 125"
